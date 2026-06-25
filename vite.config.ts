0d563f1dcdf385f2e49af29568474b182dcf0ed2
Allgaming796 <namanjha902@gmail.com>
2026-06-25 14:14:04 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: process.env.NODE_ENV === 'production' ? '/VS-Consultant-/' : '/',
+    base:  '/VS-Consultant-/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
